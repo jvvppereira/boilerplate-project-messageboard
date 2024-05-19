7fe62d5f483b04d45b6b9f7452e141b1d68c27bb
João Vítor <joao.paes@philips.com>
2024-05-19 22:12:16 +0000

    added first strucutre of tests and routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,20 @@
 'use strict';
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
+
+  app.route('/api/threads/:board')
+    .get(function (req, res) {
+      const threads = [];
+      res.json(threads);
+    });
+ 
+  app.route('/api/threads/:board')
+    .post(function (req, res) {
+      const { text, delete_password } = req.body;
+      const board = req.params.board;
+      res.json({board, text, delete_password});
+    });
+
   app.route('/api/replies/:board');
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,27 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+    test("Creating a new thread: POST request to /api/threads/{board}", function (done) {
+        chai.request(server)
+            .post("/api/threads/123")
+            .send({ board: "123", text:"content", delete_password:"secret" })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                // assert.equal(res.body.stockData.stock, "GOOG");
+                done();
+            });
+    });
+/**
+    Creating a new thread: POST request to /api/threads/{board}
+    Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
+    Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
+    Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
+    Reporting a thread: PUT request to /api/threads/{board}
+    Creating a new reply: POST request to /api/replies/{board}
+    Viewing a single thread with all replies: GET request to /api/replies/{board}
+    Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
+    Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
+    Reporting a reply: PUT request to /api/replies/{board}
+ */
+    
 });
