89b5a6342b6996a0bcddffab8a8a43cd398e6e0c
João Vítor <joao.paes@philips.com>
2024-05-25 12:55:17 +0000

    first version connected with mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,40 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+mongoose.connect(uri,clientOptions);
+
+const { Schema } = mongoose;
+
+const threadSchema = new Schema({
+  board: { type: String, required: true }, 
+  text: String,
+  delete_password: String
+});
+
+let Thread = mongoose.model('Thread', threadSchema);
+
 module.exports = function (app) {
 
   app.route('/api/threads/:board')
-    .get(function (req, res) {
-      const threads = [];
-      res.json(threads);
+    .get(async function (req, res) {
+      const board = req.params.board;
+
+      await Thread.find({ board }, data => res.json(data));
     });
  
   app.route('/api/threads/:board')
-    .post(function (req, res) {
+    .post(async function (req, res) {
       const { text, delete_password } = req.body;
       const board = req.params.board;
-      res.json({board, text, delete_password});
+
+      const document = new Thread({ board, text, delete_password });
+      await document.save().then(data => res.json(data));
+
     });
 
-  app.route('/api/replies/:board');
+  // app.route('/api/replies/:board');
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -17,17 +17,40 @@ suite('Functional Tests', function() {
                 done();
             });
     });
-/**
-    Creating a new thread: POST request to /api/threads/{board}
-    Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
-    Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
-    Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
-    Reporting a thread: PUT request to /api/threads/{board}
-    Creating a new reply: POST request to /api/replies/{board}
-    Viewing a single thread with all replies: GET request to /api/replies/{board}
-    Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
-    Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
-    Reporting a reply: PUT request to /api/replies/{board}
- */
+
+    test("Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}", function (done) {
+        done();
+    });
+
+    test("Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password", function (done) {
+        done();
+    });
+
+    test("Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password", function (done) {
+        done();
+    });
+
+    test("Reporting a thread: PUT request to /api/threads/{board}", function (done) {
+        done();
+    });
+
+    test("Creating a new reply: POST request to /api/replies/{board}", function (done) {
+        done();
+    });
+
+    test("Viewing a single thread with all replies: GET request to /api/replies/{board}", function (done) {
+        done();
+    });
+
+    test("Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password", function (done) {
+        done();
+    });
+
+    test("Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password", function (done) {
+        done();
+    });
     
+    test("Reporting a reply: PUT request to /api/replies/{board}", function (done) {
+        done();
+    });    
 });
