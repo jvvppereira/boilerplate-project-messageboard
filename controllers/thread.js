889de1f0c4cdc32242db5cbab6c88b91d4c906e2
João Vítor <joao.paes@philips.com>
2024-06-03 23:53:32 +0000

    added put/update routine and decouple find by board

--- controllers/thread.js
+++ controllers/thread.js
@@ -1,8 +1,13 @@
 const Thread = require("../models/thread.js");
 
+const getByBoard = async (board) => { //done
+    let thread = await Thread.find({ board }).exec();
+    return thread;
+}
+
 module.exports = {
 
-    async get(req, res) {
+    async get(req, res) { //TODO
         const board = req.params.board;
         const { limit, orderBy, repliesCount } = req.body;
 
@@ -14,12 +19,12 @@ module.exports = {
             data = await Thread.find(filter).limit(limit).exec();
             //:'recentFirst'
         } else {
-            data = await Thread.find(filter).exec();
+            data = await getByBoard(board);
         }
         res.json(data);
     },
 
-    async post(req, res) {
+    async post(req, res) { //done
         const board = req.params.board;
         const { text, delete_password } = req.body;
 
@@ -27,8 +32,15 @@ module.exports = {
         await document.save().then(data => res.json(data));
     },
 
-    async put(req, res) { //TODO
-        // await document.save().then(data => res.json(data));
+    async put(req, res) { //TODO TEST
+        const board = req.params.board;
+        const { text, content, delete_password } = req.body;
+        
+        const thread = await getByBoard(board);
+        const id = thread._id;
+
+        const data = await Thread.findByIdAndUpdate(id, { board, text, content, delete_password });
+        res.json(data);
     },
 
     async delete(req, res) { //TODO
